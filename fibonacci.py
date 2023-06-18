e27b607e1e807f8a82f5080841bc56b59515ba53
juhi-basava02 <136111981+juhi-basava02@users.noreply.github.com>
2023-06-18 09:37:19 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,12 @@
 #write a python program to get the fibonacci series between 0 to 50
-x,y=0,1
-
-while y<50:
-    print(y)
-    x,y = y,x+y
+num_fib = int(input("enter the range number: "))
+first_val = 0
+second_val = 1
+for n in range(0,num_fib) :
+        if(n <= 1) :
+         next = n
+        else:
+        next = first_val + second_val
+        first_val = second_val
+        second_val = next
+        print(next)  
